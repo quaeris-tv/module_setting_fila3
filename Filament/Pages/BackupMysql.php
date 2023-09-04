94aecb2830f49477da0fac575dd02aa631d9b32d
marco76tv <marco76tv@users.noreply.github.com>
2023-09-04 15:11:40 +0000

    Check & fix styling

--- Filament/Pages/BackupMysql.php
+++ Filament/Pages/BackupMysql.php
@@ -23,6 +23,7 @@ class BackupMysql extends Page
         Assert::isArray($connections = config('database.connections'));
 
         $connections = array_filter($connections, fn ($item) => 'mysql' === $item['driver']);
+
         // $connections=collect($connections)->keyBy('database');
         return ['connections' => $connections];
     }

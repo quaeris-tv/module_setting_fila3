10db325ee1c794cafa26f19738b098e359f32af7
marco76tv <marco.sottana@gmail.com>
2023-11-02 09:52:16 +0100

    up

---
+++ Database/Factories/SettingFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Setting\Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+use Modules\Setting\Models\Setting;
+
+class SettingFactory extends Factory {
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var class-string<\Illuminate\Database\Eloquent\Model>
+     */
+    protected $model = Setting::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition() {
+
+
+        return [
+            
+        ];
+    }
+}
--- Models/Setting.php
+++ Models/Setting.php
@@ -8,8 +8,21 @@ use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
 
+/**
+ * Modules\Setting\Models\Setting
+ *
+ * @property-read \Spatie\MediaLibrary\MediaCollections\Models\Collections\MediaCollection<int, \Spatie\MediaLibrary\MediaCollections\Models\Media> $media
+ * @property-read int|null $media_count
+ * @method static \Modules\Setting\Database\Factories\SettingFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder|Setting newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Setting newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Setting query()
+ * @mixin \Eloquent
+ */
 class Setting extends BaseModel implements HasMedia
 {
+    protected $fillable=[''];
+
     use InteractsWithMedia;
     // use HasUuids;
 }

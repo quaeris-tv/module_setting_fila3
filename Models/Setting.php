76495f678683a6145115ad6831356becfa99d7c7
marco76tv <marco76tv@users.noreply.github.com>
2023-11-16 15:25:11 +0000

    Check & fix styling

--- Models/Setting.php
+++ Models/Setting.php
@@ -11,12 +11,14 @@ use Spatie\MediaLibrary\InteractsWithMedia;
 /**
  * Modules\Setting\Models\Setting.
  *
- * @property-read \Spatie\MediaLibrary\MediaCollections\Models\Collections\MediaCollection<int, \Spatie\MediaLibrary\MediaCollections\Models\Media> $media
- * @property-read int|null $media_count
+ * @property \Spatie\MediaLibrary\MediaCollections\Models\Collections\MediaCollection<int, \Spatie\MediaLibrary\MediaCollections\Models\Media> $media
+ * @property int|null                                                                                                                          $media_count
+ *
  * @method static \Modules\Setting\Database\Factories\SettingFactory factory($count = null, $state = [])
- * @method static \Illuminate\Database\Eloquent\Builder|Setting newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Setting newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Setting query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Setting      newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Setting      newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Setting      query()
+ *
  * @mixin \Eloquent
  */
 class Setting extends BaseModel implements HasMedia

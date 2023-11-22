42fe0cbf914521880134f62da28b091a2e63875c
marco76tv <marco.sottana@gmail.com>
2023-11-22 10:53:21 +0100

    up

--- Models/Setting.php
+++ Models/Setting.php
@@ -15,26 +15,12 @@ use Spatie\MediaLibrary\InteractsWithMedia;
 /**
  * Modules\Setting\Models\Setting.
  *
- * @property int $id
- * @property string $group
- * @property string $name
- * @property int $locked
- * @property mixed $payload
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
- * @property-read MediaCollection<int, \Modules\Media\Models\Media> $media
+ * @property-read MediaCollection<int, Media> $media
  * @property-read int|null $media_count
  * @method static \Modules\Setting\Database\Factories\SettingFactory factory($count = null, $state = [])
  * @method static Builder|Setting newModelQuery()
  * @method static Builder|Setting newQuery()
  * @method static Builder|Setting query()
- * @method static Builder|Setting whereCreatedAt($value)
- * @method static Builder|Setting whereGroup($value)
- * @method static Builder|Setting whereId($value)
- * @method static Builder|Setting whereLocked($value)
- * @method static Builder|Setting whereName($value)
- * @method static Builder|Setting wherePayload($value)
- * @method static Builder|Setting whereUpdatedAt($value)
  * @mixin \Eloquent
  */
 class Setting extends BaseModel implements HasMedia

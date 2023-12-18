66fc9225c1aabe675341e72e3f49f79f7eb54ea5
marco76tv <marco.sottana@gmail.com>
2023-12-18 22:12:11 +0100

    first

---
+++ Database/Migrations/2023_01_01_000002_create_settings_table.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Schema\Blueprint;
+use Modules\Xot\Database\Migrations\XotBaseMigration;
+use Spatie\LaravelSettings\Migrations\SettingsMigrator;
+
+class CreateSettingsTable extends XotBaseMigration
+{
+    /**
+     * Undocumented function.
+     */
+    public function up(): void
+    {
+        // -- CREATE --
+        $this->tableCreate(
+            function (Blueprint $table): void {
+                // $table->uuid('id')->primary();
+                $table->id();
+                $table->string('group');
+                $table->string('name');
+                $table->boolean('locked')->default(false);
+                $table->json('payload');
+                $table->timestamps();
+                // $table->unique(['group', 'name']);
+            }
+        );
+        // -- UPDATE --
+        $this->tableUpdate(
+            function (Blueprint $table): void {
+                app(SettingsMigrator::class)->add('general.site_name', 'Spatie');
+                app(SettingsMigrator::class)->add('general.site_active', 'Spatie');
+                app(SettingsMigrator::class)->add('general.logo', 'logo.png');
+                // if (! $this->hasColumn('from')) {
+                //    $table->string('from')->nullable();
+                // }
+            }
+        );
+    }
+}

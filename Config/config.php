78e0fad143dae067964cf78bde8eaeb74c9ca7b1
marco76tv <marco.sottana@gmail.com>
2024-01-09 09:38:57 +0100

    up

--- Config/config.php
+++ Config/config.php
@@ -5,8 +5,5 @@ declare(strict_types=1);
 return [
     'name' => 'Setting',
     'icon' => 'heroicon-o-cog', // icon on dashboard
-<<<<<<< HEAD
     'navigation_sort' => 1,
-=======
->>>>>>> dev
 ];

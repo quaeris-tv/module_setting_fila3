8d594b8d4dd864f2910868ddc8b01155bcfdf2b9
marco <marco.sottana@gmail.com>
2024-08-05 14:37:55 +0200

    up

---
+++ Filament/Resources/DatabaseConnectionResource/Pages/ListDatabaseConnections.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Setting\Filament\Resources\DatabaseConnectionResource\Pages;
+
+use Filament\Actions;
+use Filament\Resources\Pages\ListRecords;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Modules\Setting\Filament\Actions\Table\DatabaseBackupTableAction;
+use Modules\Setting\Filament\Resources\DatabaseConnectionResource;
+use Modules\Xot\Filament\Traits\NavigationPageLabelTrait;
+
+class ListDatabaseConnections extends ListRecords
+{
+    use NavigationPageLabelTrait;
+
+    protected static string $resource = DatabaseConnectionResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            // Actions\CreateAction::make(),
+        ];
+    }
+
+    public function getTableColumns(): array
+    {
+        return [
+            TextColumn::make('name')->searchable()->sortable(),
+            TextColumn::make('driver')->searchable()->sortable(),
+            TextColumn::make('database')->searchable()->sortable(),
+        ];
+    }
+
+    public function getTableFilters(): array
+    {
+        return [
+        ];
+    }
+
+    public function getTableActions(): array
+    {
+        return [
+            // Tables\Actions\EditAction::make(),
+            DatabaseBackupTableAction::make(),
+        ];
+    }
+
+    public function getTableBulkActions(): array
+    {
+        return [
+            // Tables\Actions\BulkActionGroup::make([
+            Tables\Actions\DeleteBulkAction::make(),
+            // ]),
+        ];
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->columns($this->getTableColumns())
+            ->filters($this->getTableFilters())
+            ->actions($this->getTableActions())
+            ->bulkActions($this->getTableBulkActions());
+    }
+}
---
+++ _docs/git.txt
@@ -0,0 +1,3 @@
+aicommits
+
+https://dev.to/disukharev/opencommit-gpt-cli-to-auto-generate-impressive-commits-in-1-second-46dh

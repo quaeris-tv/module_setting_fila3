972fff46217bee45565762d48dca73be271ac2da
marco76tv <marco.sottana@gmail.com>
2025-02-11 08:51:17 +0100

    up

---
+++ App/Filament/Pages/BackupMysql.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Setting\Filament\Pages;
+
+use Filament\Pages\Page;
+use Modules\Setting\Actions\DB\DownloadAction;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Webmozart\Assert\Assert;
+
+class BackupMysql extends Page
+{
+    protected static ?string $navigationIcon = 'heroicon-o-cog';
+
+    protected static ?string $navigationGroup = 'Settings';
+
+    protected static string $view = 'setting::filament.pages.backup-mysql';
+
+    public function download(string $connectionName): BinaryFileResponse
+    {
+        return app(DownloadAction::class)->execute($connectionName);
+    }
+
+    // public function mount(): void {
+    //     $user = auth()->user();
+    //     if(!$user->hasRole('super-admin')){
+    //         redirect('/admin');
+    //     }
+    // }
+
+    protected function getViewData(): array
+    {
+        Assert::isArray($connections = config('database.connections'));
+
+        $connections = array_filter($connections, fn ($item): bool => $item['driver'] === 'mysql');
+
+        // $connections=collect($connections)->keyBy('database');
+        return ['connections' => $connections];
+    }
+}

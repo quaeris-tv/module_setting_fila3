5b1fea469b601b8080db6e4801ec3e9d7c51b987
marco76tv <marco.sottana@gmail.com>
2025-02-12 14:22:53 +0100

    up

---
+++ lang/it/setting.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'navigation' => [
+        'name' => 'Impostazione',
+        'plural' => 'Impostazioni',
+        'group' => 'Sistema',
+        'label' => 'setting',
+        'sort' => 65,
+        'icon' => 'heroicon-o-cog-6-tooth',
+    ],
+];
